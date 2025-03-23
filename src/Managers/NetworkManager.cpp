bcc68f9122b68cfccaead60836db9ec617ec6c3b
emrebarisc <22220055+emrebarisc@users.noreply.github.com>
2025-03-23 17:54:12 +0300

    Add server listener

--- src/Managers/NetworkManager.cpp
+++ src/Managers/NetworkManager.cpp
@@ -1,28 +1,72 @@
 #include "Managers/NetworkManager.h"
 
 #include <arpa/inet.h>
-#include <netinet/in.h>
+
+#include <iostream>
+#include <thread>
 
 NetworkManager::NetworkManager()
 {
 
 }
 
+NetworkManager::~NetworkManager()
+{
+	close(commandSocket_);
+	close(dataSocket_);
+
+	delete listenerThread_;
+}
+
 void NetworkManager::Init()
 {
 	commandSocket_ = socket(AF_INET, SOCK_STREAM, 0);
 
-	commandServerAddress_.sin_family = AF_INET;
-	commandServerAddress_.sin_port = htons(COMMAND_PORT);
-	commandServerAddress_.sin_addr.s_addr = inet_addr(SERVER_IP);
+	commandAddress_.sin_family = AF_INET;
+	commandAddress_.sin_port = htons(COMMAND_PORT);
+	commandAddress_.sin_addr.s_addr = inet_addr(SERVER_IP);
 
-	bind(commandSocket_, (struct sockaddr*)&commandServerAddress_, sizeof(commandServerAddress_));
+	bind(commandSocket_, (struct sockaddr*)&commandAddress_, sizeof(commandAddress_));
 
-	dataServerAddress_.sin_family = AF_INET;
-	dataServerAddress_.sin_port = htons(DATA_PORT);
-	dataServerAddress_.sin_addr.s_addr = inet_addr(SERVER_IP);
+	dataSocket_ = socket(AF_INET, SOCK_STREAM, 0);
 
-	bind(dataSocket_, (struct sockaddr*)&dataServerAddress_, sizeof(dataServerAddress_));
+	dataAddress_.sin_family = AF_INET;
+	dataAddress_.sin_port = htons(DATA_PORT);
+	dataAddress_.sin_addr.s_addr = inet_addr(SERVER_IP);
 
+	bind(dataSocket_, (struct sockaddr*)&dataAddress_, sizeof(dataAddress_));
+
+}
+
+
+void NetworkManager::StartListeningCommandAddress()
+{
+	if(!listenerThread_)
+	{
+		std::cout << "Already started listening command socket!" << std::endl;
+		return;	
+	}
+
+	listenerThread_ = new std::thread(&NetworkManager::StartListeningCommandAddressAsync, this);
+}
+
+void NetworkManager::StartListeningCommandAddressAsync()
+{
+	if(int listenResult = listen(commandSocket_, 5) != 0)
+	{
+		std::cerr << "Listen failed with code " << listenResult << std::endl;
+	}
+
+	sockaddr_in clientAddress;
+	socklen_t clientAddressSize = sizeof(clientAddress);
+	int clientSocket = accept(commandSocket_, (struct sockaddr*)&clientAddress, &clientAddressSize);
+	if(clientSocket == -1)
+	{
+		std::cerr << "Accept failed." << std::endl;
+	}
+
+	clientIP_ = clientAddress.sin_addr;
+
+	std::cout << "Connection established with " << inet_ntoa(clientIP_) << std::endl;
 }
 
--- src/Managers/NetworkManager.h
+++ src/Managers/NetworkManager.h
@@ -8,21 +8,65 @@
 
 #define SERVER_IP "192.168.31.154"
 
+namespace std
+{
+	class thread;
+}
+
+enum class Command : unsigned char
+{
+	None = 0,
+	Connect,
+	Disconnect,
+
+	StartCamera,
+	StopCamera,
+
+	SetResolution,
+	SetFPS,
+	SetPixelFormat,
+
+	StartRecording,
+	PauseRecording,
+	StopRecording,
+
+	TakeAPhotograph,
+
+	StartStream,
+	StopStream
+};
+
+class CommandMessage
+{
+	Command command;
+	char commandMassage[64];
+};
+
 class NetworkManager : public IManager
 {
 	public:
 		NetworkManager();
+		~NetworkManager();
 
 		virtual void Init() override;
+
+		void StartListeningCommandAddress();
+
 	protected:
 
 	private:
+		void StartListeningCommandAddressAsync();
+
 		static constexpr int COMMAND_PORT{ 1000 };
 		static constexpr int DATA_PORT{ 1001 };
 
 		int commandSocket_{ 0 };
 		int dataSocket_{ 0 };
 
-		sockaddr_in commandServerAddress_{ 0 };
-		sockaddr_in dataServerAddress_{ 0 };
+		sockaddr_in commandAddress_{ 0 };
+		sockaddr_in dataAddress_{ 0 };
+
+		in_addr clientIP_{ 0 };
+
+		std::thread* listenerThread_{ nullptr };
 };
